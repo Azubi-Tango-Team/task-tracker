bed4795b7a933ba5e091917e1fe414abe314ee06
keya254 <brianosiemokeya@gmail.com>
2023-04-25 18:40:05 +0300

    added css design for buttons and task cards

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Button from '../Components/Button'
-import Cardtask from '../Components/Cardtask'
+import React from "react";
+import Button from "../Components/Button";
+import Cardtask from "../Components/Cardtask";
 
 function Main() {
   return (
-    <div className='container-main'>
-    <div className='main'>
-        
-        <div className='container-task'>
-            <div><p>Total task:<Cardtask name ="3"/></p></div>
-        <Button name="Add Task" />
+    <div className="container-main">
+      <div className="main">
+        <div className="container-task">
+          <div>
+            <p>
+              Total task:3 
+            </p>
+          </div>
+          <Button name="Add Task" />
         </div>
-
-    
-    <Cardtask name ="Task 1"/>
+        <div className="card-container"> <Cardtask name="Task 1" /></div>
+        <div className="card-container"> <Cardtask name="Task 1" /></div>
+        <div className="card-container"> <Cardtask name="Task 1" /></div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
