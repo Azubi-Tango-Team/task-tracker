6413484adb042058ad6a3855e8b019b5c88bd377
keya254 <brianosiemokeya@gmail.com>
2023-04-27 23:28:54 +0300

    added some css changes on card

--- src/Components/Cardtask.js
+++ src/Components/Cardtask.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function Cardtask({name}) {
+function Cardtask({ name, date }) {
   return (
-    <div className='card-task'>{name}</div>
-  )
+    <div className="card-task">
+      <p>{name}</p> 
+      <p>{date}</p>
+      <div><FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer'}} /></div>
+    </div>
+  );
 }
 
-export default Cardtask
+export default Cardtask;
