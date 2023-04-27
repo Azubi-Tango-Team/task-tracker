605d5122c4cc4a500aef5eeeffb9b88aaebb8b06
keya254 <brianosiemokeya@gmail.com>
2023-04-27 23:58:16 +0300

    css design on x

--- src/Components/Cardtask.js
+++ src/Components/Cardtask.js
@@ -5,9 +5,12 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 function Cardtask({ name, date }) {
   return (
     <div className="card-task">
+      <div>
       <p>{name}</p> 
       <p>{date}</p>
-      <div><FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer'}} /></div>
+      </div>
+      
+      <div><FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer', fontWeight: '200em',padding:'20px'}} size="3x"/></div>
     </div>
   );
 }
